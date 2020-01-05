f80e9d4a7d37fed3c6c26ec6fe3c03d53e170ae0
Akshay K <akshaykunti1@gmail.com>
2020-01-05 06:58:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  counter = 0
+  coll = []
+  while counter <  array.size 
+    coll << yield(array[counter]).split(' ')[0]
+  end
+  coll
+end
+  
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
